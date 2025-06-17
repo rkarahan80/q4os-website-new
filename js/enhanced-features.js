ebd3ff396c1749102ea3f10724b97d6233a7d572
rkarahan80 <rkarahan83@gmail.com>
2025-06-17 11:41:50 +0300

    Fix setupDownloadTracker method error

--- js/enhanced-features.js
+++ js/enhanced-features.js
@@ -12,7 +12,6 @@ class Q4OSEnhancer {
         this.setupProgressBars();
         this.setupSearchFunctionality();
         this.setupThemeToggle();
-        this.setupDownloadTracker();
         this.setupNewsletterSignup();
         this.setupDonationSystem();
         this.setupNavbarScroll();
